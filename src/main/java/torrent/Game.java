1e3954b547ed347ca138c072a237832992c42344
Torrent <torrent@wellsfargo.com>
2022-04-07 17:43:02 -0400

    Add 4 classes to quell errors

---
+++ src/main/java/torrent/Game.java
@@ -0,0 +1,27 @@
+package torrent;
+
+import java.util.*;
+
+public class Game {
+    
+
+    public void init(){
+
+    }
+
+    public Map getMap(){
+        return null;
+    }
+
+    public Player getPlayer(){
+        return null;
+    }
+
+    public GameState getState(){
+        return null;
+    }
+
+    public String getSword(){
+        return null;
+    }
+}
