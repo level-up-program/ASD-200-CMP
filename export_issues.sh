e5790227fdc279e23a1463b7679c8f985289bf24
JP White <jpwhite3@gmail.com>
2022-08-25 12:12:25 -0400

    Updated issue export scripts

--- export_issues.sh
+++ export_issues.sh
@@ -1,6 +1,8 @@
 MIN_NUM=106
-MAX_NUM=`gh issue list --limit 1 --json number | jq ".[].number"`; 
+MAX_NUM=`gh issue list --state all --limit 1 --json number | jq ".[].number"`;
+rm -f github-dump.json
 for n in `seq $MIN_NUM $MAX_NUM`; do 
-	gh issue view $n --json assignees,author,body,closed,closedAt,comments,createdAt,id,labels,milestone,number,projectCards,reactionGroups,state,title,updatedAt,url >> github-dump.json; 
+	gh issue view $n --json id,number,state,title,author,body,createdAt,closedAt >> github-dump.json; 
 done
 
+python3 json2csv.py
--- json2csv.py
+++ json2csv.py
@@ -1,38 +1,29 @@
-# Python program to convert
-# JSON file to CSV
-
-
 import json
 import csv
 
-
-# Opening JSON file and loading the data
-# into the variable data
-with open('github-dump.json') as json_file:
+with open("github-dump.json") as json_file:
     data = []
     for line in json_file.readlines():
-	data.append(json.loads(line))
+        data.append(json.loads(line))
 
-# now we will open a file for writing
-data_file = open('github-dump.csv', 'w')
-
-# create the csv writer object
+data_file = open("github-dump.csv", "w")
 csv_writer = csv.writer(data_file)
 
-# Counter variable used for writing
-# headers to the CSV file
 count = 0
-
 for issue in data:
-	if count == 0:
 
-		# Writing headers of CSV file
-		header = issue.keys()
-		csv_writer.writerow(header)
-		count += 1
+    if issue["title"] != "ACCESS REQUEST":
+        continue
 
-	# Writing data of CSV file
-	csv_writer.writerow(issue.values())
+    issue["author"] = issue["author"]["login"]
+
+    # Writing headers of CSV file
+    if count == 0:
+        header = issue.keys()
+        csv_writer.writerow(header)
+        count += 1
+
+    # Writing data of CSV file
+    csv_writer.writerow(issue.values())
 
 data_file.close()
-
